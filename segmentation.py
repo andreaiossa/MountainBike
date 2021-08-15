3e2079324d57ff7dd7cd913970854f5c705403b7
yoza <andreaiossa97@gmail.com>
2021-08-15 18:02:52 +0200

    formatted segmentation.py and test.py, added histogram matching in HSV color space

--- segmentation.py
+++ segmentation.py
@@ -2,56 +2,58 @@ from cv2 import cv2
 import numpy as np
 from detectron2 import model_zoo
 from detectron2.config import get_cfg
-from detectron2.data import MetadataCatalog
 from detectron2.engine import DefaultPredictor
-from detectron2.utils.visualizer import Visualizer
-import argparse
-import time
-import os
+from os.path import isfile
 from PIL import Image
 
-im = cv2.imread("./files/immagini/back_green.jpg")
-w = int(im.shape[1] / 3)
-h = int(im.shape[0] / 3)
-im = cv2.resize(im, (w, h))
-# cv2.imshow("a", im_resized)
 
-# cfg = get_config("Misc/scratch_mask_rcnn_R_50_FPN_3x_gn.yaml", trained=True)
-# cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5
+def create_predictor():
+    print("Generating predicor...")
+    cfg = get_cfg()
+    if (isfile("./config.yaml")):
+        cfg.merge_from_file("./config.yaml")
+    else:
+        cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
+        cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
+        cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5
 
-cfg = get_cfg()
-cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
-cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # set threshold for this model
-cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
+    with open("config.yaml", "w") as f:
+        f.write(cfg.dump())
 
-predictor = DefaultPredictor(cfg)
-outputs = predictor(im)
+    predictor = DefaultPredictor(cfg)
+    print("Done")
+    return predictor
 
-# print(outputs["instances"].pred_classes)
-# print(outputs["instances"].pred_masks)
-mask = outputs["instances"].pred_masks[0]
 
-mask = np.array(mask.cpu(), dtype=np.uint8)
-mask2 = mask
-mask = Image.fromarray(mask * 255)
-mask.save("mask.jpg")
-mask = cv2.imread("mask.jpg", cv2.IMREAD_GRAYSCALE)
-cv2.imwrite("mask.jpg", mask)
+def compute_mask(image, predictor, refined=False, visualize=False):
+    print("computing rough mask...")
+    im = cv2.imread(image)
+    outputs = predictor(im)
 
-mask[mask > 0] = cv2.GC_PR_FGD
-mask[mask == 0] = cv2.GC_BGD
-fgModel = np.zeros((1, 65), dtype="float")
-bgModel = np.zeros((1, 65), dtype="float")
+    print("classes found: {}".format(outputs["instances"].pred_classes))
 
-roughOutput = cv2.bitwise_and(im, im, mask=mask)
+    mask = outputs["instances"].pred_masks[0]
+    mask = np.array(mask.cpu(), dtype=np.uint8)
 
-(mask, bgModel, fgModel) = cv2.grabCut(im, mask, None, bgModel, fgModel, iterCount=1, mode=cv2.GC_INIT_WITH_MASK)
+    if (refined):
+        print("refining mask...")
+        mask = refine(mask, im)
+        print("done")
 
-outputMask = np.where((mask == cv2.GC_BGD) | (mask == cv2.GC_PR_BGD), 0, 1)
-outputMask = (outputMask * 255).astype("uint8")
-roughOutput = cv2.bitwise_and(im, im, mask=mask2)
-output = cv2.bitwise_and(im, im, mask=outputMask)
+    return mask
 
-cv2.imshow("Rough Output", roughOutput)
-cv2.imshow("Output", output)
-cv2.waitKey(0)
+
+def refine(mask, im):
+    mask = Image.fromarray(mask * 255)
+    mask.save("./files/temp/mask.jpg")
+    mask = cv2.imread("./files/temp/mask.jpg", cv2.IMREAD_GRAYSCALE)
+    mask[mask > 0] = cv2.GC_PR_FGD
+    mask[mask == 0] = cv2.GC_BGD
+    fgModel = np.zeros((1, 65), dtype="float")
+    bgModel = np.zeros((1, 65), dtype="float")
+
+    (mask, bgModel, fgModel) = cv2.grabCut(im, mask, None, bgModel, fgModel, iterCount=1, mode=cv2.GC_INIT_WITH_MASK)
+    outputMask = np.where((mask == cv2.GC_BGD) | (mask == cv2.GC_PR_BGD), 0, 1)
+    outputMask = (outputMask * 255).astype("uint8")
+
+    return outputMask
--- test.py
+++ test.py
@@ -1,25 +1,99 @@
 from cv2 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
+import segmentation
 
-cap = cv2.VideoCapture('./videos/20210701_160705.mp4')
-fgbg = cv2.createBackgroundSubtractorMOG2(varThreshold=100)
-counter = 0
 
-while (1):
-    counter += 1
-    ret, frame = cap.read()
-    fgmask = fgbg.apply(frame)
+def background(video, start, end, tresh, show=False):
+    cap = cv2.VideoCapture(video)
+    length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    print(length)
+    cap.set(1, start)
+    fgbg = cv2.createBackgroundSubtractorMOG2(varThreshold=tresh)
+    counter = 0
 
-    frame = cv2.resize(frame, (960, 540))
-    fgmask = cv2.resize(fgmask, (960, 540))
+    img = False
+    mask = False
 
-    cv2.imshow('fgmask', fgmask)
-    cv2.imshow('frame', frame)
+    while (1):
+        counter += 1
+        print(counter)
+        ret, frame = cap.read()
+        fgmask = fgbg.apply(frame)
 
-    k = cv2.waitKey(30) & 0xff
-    if k == 27:
-        break
+        if show:
+            frameR = cv2.resize(frame, (960, 540))
+            fgmaskR = cv2.resize(fgmask, (960, 540))
+            cv2.imshow('fgmask', fgmaskR)
+            cv2.imshow('frame', frameR)
 
-cap.release()
-cv2.destroyAllWindows()
+        if counter == end:
+            img = frame
+            mask = fgmask
+            break
+
+        k = cv2.waitKey(30) & 0xff
+        if k == 16:
+            break
+
+    if show:
+        cap.release()
+        cv2.destroyAllWindows()
+
+    return img, mask
+
+
+def remove_noise(mask, radius, iteration1, iteration2):
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (radius, radius))
+    mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel, iterations=iteration1)
+    mask = cv2.morphologyEx(mask, cv2.MORPH_DILATE, kernel, iterations=iteration2)
+
+    return mask
+
+
+def showImg(images, scale):
+    counter = 0
+    for image in images:
+        counter += 1
+        w = int(image.shape[1] / scale)
+        h = int(image.shape[0] / scale)
+        resized = cv2.resize(image, (w, h))
+        cv2.imshow('image {}'.format(counter), resized)
+    cv2.waitKey(0)
+
+
+def compute_hist(reference, img, mask, metric, show=False):
+    static_image = cv2.imread(reference)
+    predictor = segmentation.create_predictor()
+    static_mask = segmentation.compute_mask(reference, predictor, refined=True)
+    cut1 = cv2.bitwise_and(static_image, static_image, mask=static_mask)
+    cut2 = cv2.bitwise_and(img, img, mask=mask)
+
+    static_image_HSV = cv2.cvtColor(static_image, cv2.COLOR_BGR2HSV)
+    img_HSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    channels = [0, 1]
+    h_ranges = [0, 180]
+    s_ranges = [0, 256]
+    ranges = h_ranges + s_ranges
+
+    static_hist = cv2.calcHist([static_image_HSV], channels, static_mask, [8, 8], ranges, accumulate=False)
+    static_hist = cv2.normalize(static_hist, static_hist, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX)
+
+    blob_hist = cv2.calcHist([img_HSV], channels, mask, [8, 8], ranges, accumulate=False)
+    blob_hist = cv2.normalize(blob_hist, blob_hist, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX)
+
+    cut3 = cv2.bitwise_and(static_image_HSV, static_image_HSV, mask=static_mask)
+    cut4 = cv2.bitwise_and(img_HSV, img_HSV, mask=mask)
+
+    if show:
+        showImg([static_image, static_mask, cut1, cut2, cut3, cut4], 3)
+
+    result = cv2.compareHist(blob_hist, static_hist, metric)
+    return result
+
+
+img, mask = background('./files/videos/single_green_jump.mp4', 1400, 45, 100, show=True)
+mask = remove_noise(mask, 5, 1, 1)
+result = compute_hist("./files/imgs/blue_front.jpg", img, mask, cv2.HISTCMP_CHISQR, show=False)
+
+print(result)
