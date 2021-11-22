added tests for saliency, added softmax implementation, fixed mixing softmax for some metrics in histogram comparison

hist.py
@@ -16,8 +16,8 @@ def compareHistCV(hist1, hist2, metric):
     Gives the result of the comparison of two histograms with defined metric.
     METRICS: CV_COMP_CORREL (best is higher), CV_COMP_INTERSECT (best is higher), CV_COMP_CHISQR (best is lower), CV_COMP_BHATTACHARYYA (best is lower)
     '''
-    result = cv2.compareHist(hist1.flatten(), hist2.flatten(), metric)
-    return result
+
+    return cv2.compareHist(hist1.flatten(), hist2.flatten(), metric)
 
 
 def compareHistPY(hist1, hist2, metric):
@@ -25,11 +25,11 @@ def compareHistPY(hist1, hist2, metric):
     Gives the result of the comparison of two histograms with defined metric.
     METRICS: CV_COMP_CORREL (best is higher), CV_COMP_INTERSECT (best is higher), CV_COMP_CHISQR (best is lower), CV_COMP_BHATTACHARYYA (best is lower)
     '''
-    result = metric(hist1.flatten(), hist2.flatten())
-    return result
+
+    return metric(hist1.flatten(), hist2.flatten())
 
 
-def compute2DHist(img, mask=None, normalize=False, difference=False, pixels=None):
+def compute2DHist(img, mask=None, normalize=False, difference=False):
     '''
     Given img (BGR) and mask return the 8 bins histogram in HS color space
     '''
@@ -89,7 +89,13 @@ def compute1DHist(img, mask=None, normalize=False):
         cv2.normalize(histH, histH, norm_type=normalize)
         cv2.normalize(histS, histS, norm_type=normalize)
 
-    return (histH, histS)
+    return histH
+
+
+def softMaxHist(hist):
+    histExp = np.exp(hist)
+
+    return histExp / histExp.sum()
 
 
 def displayHist(hist, fig, mod=1):
@@ -169,6 +175,9 @@ def fullHistComp(riders, fileName, channels=1, show=False):
         mod = metricTuple[1]
         metricName = metricTuple[2]
         fun = metricTuple[3]
+        softmax = False
+        if len(metricTuple) == 5:
+            softmax = True
         scoreIn1 = 0
         scoreIn3 = 0
         scoreIn5 = 0
@@ -176,9 +185,6 @@ def fullHistComp(riders, fileName, channels=1, show=False):
         for ref in riders:
             tmpRow = []
             row = [ref.name]
-            minimum = None
-            maximum = None
-            match = False
             shouldMatch = None
             results = []
             for rider in riders:
@@ -195,6 +201,9 @@ def fullHistComp(riders, fileName, channels=1, show=False):
                     fig1 = plt.figure(rider.name)
                     displayHist(riderHist, fig1, mod=1)
                     plt.show()
+                if softmax:
+                    refHist = softMaxHist(refHist)
+                    riderHist = softMaxHist(riderHist)
                 if fun == "CV":
                     result = compareHistCV(riderHist, refHist, metric)
                 elif fun == "PY":

test.py
@@ -1,5 +1,6 @@
 import time
 import cv2
+import cv2 as cv
 import numpy as np
 from matplotlib import pyplot as plt
 import preprocess
@@ -7,6 +8,8 @@ import pickle
 import sys
 from tabulate import tabulate
 import hist
+import videoParsing
+from scipy.spatial import distance
 
 ### CREATE EMPYT RIDERS FROM FOLDER
 
@@ -28,26 +31,30 @@ riders = sorted(riders, key=lambda x: int(x.name.split("RIDER")[1]))
 #     rider.squashHist(mod="median")
 #     preprocess.updateRider(rider)
 
-# ### COMPARISON
+# --------------------------------------------------------------------------------------- COMPARISON ------------------------------------------------------------------------------------------------------------------------------
 
 # hist.fullHistComp(riders, "bSub_8_H_L2.txt")
 # hist.fullHistComp(riders, "bSub_8-8_HS_L2.txt", channels=2)
 
-cv2.imshow("c", riders[0].bgCustom)
-cv2.imshow("ca", riders[0].bgBack)
-cv2.waitKey(0)
+# cv2.imshow("c", riders[0].bgCustom)
+# cv2.imshow("ca", riders[0].bgBack)
+# cv2.waitKey(0)
 
-### FRAMES
+# for rider in riders:
+#     videoParsing.saveVideoBGSub(rider)
+
+# ----------------------------------------------------------------------------------------   FRAMES   -----------------------------------------------------------------------------------------------------------------------------
 
 # # print(len(riders[1].frameAndMasksCustom))
-# # preprocess.checkMasks(riders[1].frameAndMasksCustom)
+# preprocess.checkMasks(riders[1].frameAndMasksCustom)
+# preprocess.checkMasks(riders[1].frameAndMasksBack)
 # # # # # print(len(riders[1].frameAndMasksBack))
 # # # preprocess.checkMasks(riders[1].frameAndMasksBack)
 
 # # cv2.imshow("test", zero.frameAndMasksCustomFull[0][0])
 # # cv2.waitKey(0)
 
-### HISTOGRAMS
+#  --------------------------------------------------------------------------------------   HISTOGRAMS   --------------------------------------------------------------------------------------------------------------------------
 
 # a = pickle.load(open("./files/pickles/RIDER2.p", "rb"))
 # b = pickle.load(open("./files/pickles/RIDER1.p", "rb"))
@@ -56,12 +63,59 @@ cv2.waitKey(0)
 # print(a.bgBack.shape)
 # print(a.bgCustom.shape)
 
-print(riders[1].customHist1D)
+# print(riders[1].customHist1D)
 
-figA = plt.figure()
-axA = figA.add_subplot()
+# figA = plt.figure()
+# axA = figA.add_subplot()
 
-axA.plot(riders[1].backHist1D, color="black")
-axA.plot(riders[1].customHist1D, color="r")
-axA.plot(riders[0].customHist1D, color="b")
-plt.show()
+# axA.plot(riders[1].backHist1D, color="black")
+# axA.plot(riders[1].customHist1D, color="r")
+# axA.plot(riders[0].customHist1D, color="b")
+# plt.show()
+
+# -------------------------------------------------------------------------------------------  SALIENCY  --------------------------------------------------------------------------------------------------------------------------
+
+f, m = riders[1].frameAndMasksBack[20]
+image = cv2.bitwise_and(f, f, mask=m)
+hsv_img = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+saliency = cv2.saliency.StaticSaliencyFineGrained_create()
+(success, saliencyMap) = saliency.computeSaliency(image)
+saliencyMap = (saliencyMap * 255).astype("uint8")
+threshMap = cv2.threshold(saliencyMap.astype("uint8"), 200, 255, cv2.THRESH_BINARY)[1]
+
+backThresh = cv2.inRange(hsv_img, (0, 150, 150), (180, 255, 255))
+backCut = cv2.bitwise_and(image, image, mask=backThresh)
+backHist = hist.compute1DHist(f, mask=backThresh, normalize="density")
+
+cv2.imshow("Image", image)
+# cv2.imshow("Output", saliencyMap)
+# cv2.imshow("Thresh", threshMap)
+cv2.imshow("S", backCut)
+
+cF, cM = riders[1].frameAndMasksCustom[15]
+customImage = cv2.bitwise_and(cF, cF, mask=cM)
+customHsv_img = cv2.cvtColor(customImage, cv2.COLOR_BGR2HSV)
+
+customThresh = cv2.inRange(customHsv_img, (0, 100, 100), (180, 255, 255))
+customCut = cv2.bitwise_and(customImage, customImage, mask=customThresh)
+customHist = hist.compute1DHist(cF, mask=customThresh, normalize="density")
+
+cv2.imshow("Image C", image)
+cv2.imshow("S C", customCut)
+# cv2.waitKey(0)
+
+decoy = cv2.imread("files/imgs/green_front.jpg")
+decoyHsv_img = cv2.cvtColor(decoy, cv2.COLOR_BGR2HSV)
+
+decoyThresh = cv2.inRange(decoyHsv_img, (0, 100, 100), (180, 255, 255))
+decoyCut = cv2.bitwise_and(decoy, decoy, mask=decoyThresh)
+decoyHist = hist.compute1DHist(decoy, mask=decoyThresh, normalize="density")
+
+cv2.imshow("decoy", decoy)
+cv2.imshow("decoy c", decoyCut)
+cv2.waitKey(0)
+
+result = hist.compareHistCV(hist.softMaxHist(backHist), hist.softMaxHist(customHist), cv2.HISTCMP_BHATTACHARYYA)
+result2 = hist.compareHistCV(hist.softMaxHist(backHist), hist.softMaxHist(decoyHist), cv2.HISTCMP_BHATTACHARYYA)
+print(result, result2)