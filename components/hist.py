fixed pipeline, added weighting by position in pipeline

components/hist.py
@@ -4,7 +4,7 @@ import matplotlib
 from matplotlib import pyplot as plt
 from scipy.spatial import distance
 import sys
-import utils
+from components.utils import *
 import seaborn as sns
 from tabulate import tabulate
 
@@ -33,7 +33,7 @@ def compute2DHist(img, mask=None, normalize=False, difference=False):
     Given img (BGR) and mask return the 8 bins histogram in HS color space
     '''
     if isinstance(mask, bool):
-        print(f"{utils.bcolors.presetWARNING} Impossible to compute histograms, given mask is empty")
+        print(f"{bcolors.presetWARNING} Impossible to compute histograms, given mask is empty")
         return
 
     img = cv2.imread(img) if isinstance(img, str) else img
@@ -66,7 +66,7 @@ def compute1DHist(img, mask=None, normalize=False):
     Given img (BGR) and mask return the 8 bins histogram for H and for S of HSV (separatly)
     '''
     if isinstance(mask, bool):
-        print(f"{utils.bcolors.presetWARNING} Impossible to compute histograms, given mask is empty")
+        print(f"{bcolors.presetWARNING} Impossible to compute histograms, given mask is empty")
         return
 
     img = cv2.imread(img) if isinstance(img, str) else img
@@ -125,7 +125,7 @@ def squashHists(hists, mod="median"):
     shape = hists[0].shape
     for hist in hists:
         if hist.shape != shape:
-            print(f"{utils.bcolors.presetERROR} Histograms need to be of same shape (bins) to average them")
+            print(f"{bcolors.presetERROR} Histograms need to be of same shape (bins) to average them")
             return
     outHist = np.zeros(shape)
     if mod == "median":
@@ -152,7 +152,7 @@ def squashHists(hists, mod="median"):
 def diffHist(hist1, hist2):
 
     if hist1.shape != hist2.shape:
-        print(f"{utils.bcolors.presetERROR} Histograms need to be of same shape (bins) to difference them")
+        print(f"{bcolors.presetERROR} Histograms need to be of same shape (bins) to difference them")
         return
     shape = hist1.shape
     outHist = np.zeros(shape)

pipeline.py
@@ -2,16 +2,16 @@ import sys
 import numpy as np
 from matplotlib import pyplot as plt
 from tabulate import tabulate
+from sklearn.preprocessing import MinMaxScaler
 from components.utils import *
 from components.hist import *
 
-def fullHistComp(riders, fileName, channels=1, show=False):
+def fullHistComp(riders, fileName, channels=1, show=False, position=False):
     tables = []
     headers = []
     for rider in riders:
         headers.append(rider.name)
-
-    for metricTuple in utils.metrics:
+    for metricTuple in metrics:
         metric = metricTuple[0]
         mod = metricTuple[1]
         metricName = metricTuple[2]
@@ -22,22 +22,26 @@ def fullHistComp(riders, fileName, channels=1, show=False):
         scoreIn1 = 0
         scoreIn3 = 0
         scoreIn5 = 0
+        riderIndex = 0
         table = []
-        for ref in riders:
+        for rider in riders:
+            riderIndex += 1
             tmpRow = []
-            row = [ref.name]
+            row = [rider.name]
             shouldMatch = None
             results = []
-            refHists = ref.squashHists
-            refFeature = ref.resNetFeatures
-            for rider in riders:
-                riderHists = rider.squashHists
-                riderFeature = rider.resNetFeatures
+            riderHists = rider.squashHists
+            riderFeature = rider.resNetFeatures
+            refIndex = 0
+            for ref in riders:
+                refIndex += 1
+                refHists = ref.squashHists
+                refFeature = ref.resNetFeatures
                 if channels == 1:
                     refHist = np.float32(refHists["back"][0])
-                    riderHist = np.float32(refHists["custom"][0])
+                    riderHist = np.float32(riderHists["custom"][0])
                 if channels == 2:
-                    refHist = np.float32(riderHists["custom"][1])
+                    refHist = np.float32(refHists["custom"][1])
                     riderHist = np.float32(riderHists["back"][1])               
                 if channels == "feature":
                     refHist = np.float32(refFeature["back"])
@@ -58,6 +62,11 @@ def fullHistComp(riders, fileName, channels=1, show=False):
                     result = compareHistCV(riderHist, refHist, metric)
                 elif fun == "PY":
                     result = compareHistPY(riderHist, refHist, metric)
+                if position:
+                    result = minMax(result)
+                    indexDistance = abs(refIndex - riderIndex) / 10
+                    result = position*result + (1-position)*indexDistance
+
                 results.append((rider.name, result))
                 if rider.name == ref.name:
                     shouldMatch = result
@@ -75,14 +84,14 @@ def fullHistComp(riders, fileName, channels=1, show=False):
 
             for r in tmpRow:
                 if r == shouldMatch and r == best:
-                    row.append(f"{utils.bcolors.OKBLUE}{r}{utils.bcolors.ENDC}")
+                    row.append(f"{bcolors.OKBLUE}{r}{bcolors.ENDC}")
                 elif r == best:
-                    row.append(f"{utils.bcolors.OKCYAN}{r}{utils.bcolors.ENDC}")
+                    row.append(f"{bcolors.OKCYAN}{r}{bcolors.ENDC}")
                 else:
                     row.append(r)
             table.append(row)
 
-        table.append([f"{utils.bcolors.OKGREEN}{metricName}", f"Score in 1: {scoreIn1}/10", f"Score in 3: {scoreIn3}/10", f"Score in 5: {scoreIn5}/10 " f"better is {mod}{utils.bcolors.ENDC}"])
+        table.append([f"{bcolors.OKGREEN}{metricName}", f"Score in 1: {scoreIn1}/10", f"Score in 3: {scoreIn3}/10", f"Score in 5: {scoreIn5}/10 " f"better is {mod}{bcolors.ENDC}"])
         tables.append(table)
 
     original_stdout = sys.stdout
@@ -93,13 +102,13 @@ def fullHistComp(riders, fileName, channels=1, show=False):
             print("\n")
         sys.stdout = original_stdout
 
-def fullHistCompHelmet(riders, fileName, coeff=0.5):
+def fullHistCompHelmet(riders, fileName, coeff=0.5, position=False):
     tables = []
     headers = []
     for rider in riders:
         headers.append(rider.name)
 
-    for metricTuple in utils.metrics:
+    for metricTuple in metrics:
         metric = metricTuple[0]
         mod = metricTuple[1]
         metricName = metricTuple[2]
@@ -110,15 +119,19 @@ def fullHistCompHelmet(riders, fileName, coeff=0.5):
         scoreIn1 = 0
         scoreIn3 = 0
         scoreIn5 = 0
+        riderIndex = 0
         table = []
-        for ref in riders:
+        for rider in riders:
+            riderIndex += 1
             tmpRow = []
-            row = [ref.name]
+            row = [rider.name]
             shouldMatch = None
             results = []
-            refHists = ref.maxHists
-            for rider in riders:
-                riderHists = rider.maxHists
+            riderHists = rider.maxHists
+            refIndex = 0
+            for ref in riders:
+                refIndex += 1
+                refHists = ref.maxHists
                 refHistHelmet = np.float32(refHists["back"][0])
                 refHistBody = np.float32(refHists["back"][1])
                 riderHistHelmet = np.float32(riderHists["custom"][0])
@@ -137,6 +150,10 @@ def fullHistCompHelmet(riders, fileName, coeff=0.5):
                     resultHelmet = compareHistPY(riderHistHelmet, refHistHelmet, metric)
                     resultBody = compareHistPY(riderHistBody, refHistBody, metric)
                     result = (1-coeff)*resultHelmet + coeff*resultBody
+                if position:
+                    result = minMax(result)
+                    indexDistance = abs(refIndex - riderIndex) / 10
+                    result = position*result + (1-position)*indexDistance
                 results.append((rider.name, result))
                 if rider.name == ref.name:
                     shouldMatch = result
@@ -154,15 +171,14 @@ def fullHistCompHelmet(riders, fileName, coeff=0.5):
 
             for r in tmpRow:
                 if r == shouldMatch and r == best:
-                    row.append(f"{utils.bcolors.OKBLUE}{r}{utils.bcolors.ENDC}")
+                    row.append(f"{bcolors.OKBLUE}{r}{bcolors.ENDC}")
                 elif r == best:
-                    row.append(f"{utils.bcolors.OKCYAN}{r}{utils.bcolors.ENDC}")
+                    row.append(f"{bcolors.OKCYAN}{r}{bcolors.ENDC}")
                 else:
                     row.append(r)
             table.append(row)
 
-
-        table.append([f"{utils.bcolors.OKGREEN}{metricName}", f"Score in 1: {scoreIn1}/10", f"Score in 3: {scoreIn3}/10", f"Score in 5: {scoreIn5}/10 " f"better is {mod}{utils.bcolors.ENDC}"])
+        table.append([f"{bcolors.OKGREEN}{metricName}", f"Score in 1: {scoreIn1}/10", f"Score in 3: {scoreIn3}/10", f"Score in 5: {scoreIn5}/10 " f"better is {mod}{bcolors.ENDC}"])
         tables.append(table)
 
     original_stdout = sys.stdout

test.py
@@ -17,6 +17,10 @@ from components.segmentation import *
 from caffe.caffe2py.caffenet import *
 
 
+from sklearn.preprocessing import MinMaxScaler
+data = [1,23,53,2,0.1]
+print(min(data))
+
 ### CREATE EMPYT RIDERS FROM FOLDER
 
 # RIDERS = preprocess.collectRidersFolders()
@@ -45,7 +49,7 @@ from caffe.caffe2py.caffenet import *
 # # --------------------------------------------------------------------------------------- COMPARISON ------------------------------------------------------------------------------------------------------------------------------
 
 # hist.fullHistComp(riders, "features.txt", channels="feature")
-model = resNetIstantiateModel()
+# model = resNetIstantiateModel()
 
 import torch
 import torch.nn as nn
@@ -75,13 +79,13 @@ from torchsummary import summary
 #         x = torch.squeeze(x)
 #         x = self.classifier(x) #use our classifier.
 #         return x
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-print(device)
-modelA = models.resnet50(pretrained=False)
-modelA.to(device)
-modelB = resNetIstantiateModel()
-modelB.to(device)
-summary(modelB,(3,224,224))
+# device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+# print(device)
+# modelA = models.resnet50(pretrained=False)
+# modelA.to(device)
+# modelB = resNetIstantiateModel()
+# modelB.to(device)
+# summary(modelB,(3,224,224))
 
 # hist.fullHistComp(riders, "bSub_8-8_HS_L2.txt", channels=2)
 